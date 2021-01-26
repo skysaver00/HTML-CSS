60486fdcb830d5566c1084f042a17ebc9978b139
skysaver00 <skysaver00@naver.com>
2021-01-26 15:41:29 +0900

    2021-01-26

--- ProjectB_Color_Flipper/app.js
+++ ProjectB_Color_Flipper/app.js
@@ -1,8 +1,18 @@
-const colors = ["green", "red"];
+const colors = ["green", "red", "yellow", "blue", "black"];
 
-const btn = doucment.getElementById('btn');
+const btn = doucment.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener('click', function() {
-    console.log(document.body);
+btn.addEventListener("click", function() {
+    //get random number betweeon 0 - 3 colors[]
+
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    doucment.body.style.backgroundColor = colors[randomNumber];
+
+    color.textContent = color[randomNumber];
 });
+
+function getRandomNumber() {
+    return Math.random();
+}
