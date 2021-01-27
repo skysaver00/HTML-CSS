29119659670d9c5c68057552ae2183b4ce453346
skysaver00 <skysaver00@naver.com>
2021-01-27 16:45:04 +0900

    2021-01-27

---
+++ ProjectC_Counter/app.js
@@ -0,0 +1,34 @@
+//set initial count
+let count = 0;
+
+//select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function(e) {
+        const styles = e.currentTarget.classList;
+        value.style.color = 'black';
+
+        if(styles.contains('decrease')) {
+            count -= 1;
+        }
+        if(styles.contains('reset')) {
+            count = 0;
+        }
+        if(styles.contains('increase')) {
+            count += 1;
+        }
+
+        if(count > 0) {
+            value.style.color = 'green';
+        }
+        else if(count == 0) {
+            value.style.color = 'black';
+        }
+        else {
+            value.style.color = 'red';
+        }
+        value.textContent = count;
+    })
+});
