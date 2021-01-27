758618716db5300afdd9db0bf3221df03c88a1e4
skysaver00 <skysaver00@naver.com>
2021-01-27 16:45:45 +0900

    2021-01-27

--- ProjectC_Counter/app.js
+++ ProjectC_Counter/app.js
@@ -8,7 +8,6 @@ const btns = document.querySelectorAll('.btn');
 btns.forEach(function (btn) {
     btn.addEventListener('click', function(e) {
         const styles = e.currentTarget.classList;
-        value.style.color = 'black';
 
         if(styles.contains('decrease')) {
             count -= 1;
