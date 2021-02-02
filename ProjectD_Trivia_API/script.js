610dc89a46d25b3649598c74498b073d50f6ba1d
skysaver00 <skysaver00@naver.com>
2021-02-02 22:21:07 +0900

    2021-02-02

---
+++ ProjectD_Trivia_API/script.js
@@ -0,0 +1,27 @@
+window.onload = sendApiRequest;
+
+async function sendApiRequest() {
+    let response = await fetch(`https://opentdb.com/api.php?amount=1&type=multiple`);
+    console.log(response);
+    let data = await response.json();
+    console.log(data);
+    useApiData(data);
+}
+
+function useApiData(data) {
+    document.querySelector("#category").innerHTML = `Category: ${data.results[0].category}`
+    document.querySelector("#difficulty").innerHTML = `Difficulty: ${data.results[0].difficulty}`
+    document.querySelector("#question").innerHTML = `Question: ${data.results[0].question}`
+    
+    document.querySelector("#answer1").innerHTML = data.results[0].correct_answer
+    document.querySelector("#answer2").innerHTML = data.results[0].incorrect_answers[0]
+    document.querySelector("#answer3").innerHTML = data.results[0].incorrect_answers[1]
+    document.querySelector("#answer4").innerHTML = data.results[0].incorrect_answers[2]    
+}
+
+let correctButton = document.querySelector("#answer1")
+
+correctButton.addEventListener("click", () => {
+    alert("정답입니다! 와 짝짝짝짝짝 X 5")
+    sendApiRequest()
+})
