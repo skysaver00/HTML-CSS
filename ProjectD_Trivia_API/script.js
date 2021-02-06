396292623cc71385d0a2f2292f7d7d1c8c601ee2
skysaver00 <skysaver00@naver.com>
2021-02-06 22:07:51 +0900

    2021-02-06

--- ProjectD_Trivia_API/script.js
+++ ProjectD_Trivia_API/script.js
@@ -8,13 +8,16 @@ async function sendApiRequest() {
     useApiData(data);
 }
 
+var rand;
+let correctButton;
+
 function useApiData(data) {
     document.querySelector("#category").innerHTML = `Category: ${data.results[0].category}`
     document.querySelector("#difficulty").innerHTML = `Difficulty: ${data.results[0].difficulty}`
     document.querySelector("#question").innerHTML = `Question: ${data.results[0].question}`
     
 
-    var rand = Math.floor(Math.random() * 4) + 1;
+    rand = Math.floor(Math.random() * 4) + 1;
 
     var correct = "#answer" + rand;
     console.log(correct);
@@ -22,15 +25,16 @@ function useApiData(data) {
     var incorrect = 0;
     for(var i = 1; i <= 4; i++) {
         if(i == rand) continue;
-        document.querySelector("#answer"+i).innerHTML = 
+        document.querySelector("#answer"+i).innerHTML = data.results[0].incorrect_answers[incorrect];
+        incorrect++;
     }
-    document.querySelector("#answer1").innerHTML = data.results[0].correct_answer
-    document.querySelector("#answer2").innerHTML = data.results[0].incorrect_answers[0]
-    document.querySelector("#answer3").innerHTML = data.results[0].incorrect_answers[1]
-    document.querySelector("#answer4").innerHTML = data.results[0].incorrect_answers[2]    
+    document.querySelector(correct).innerHTML = data.results[0].correct_answer;
+    
+    correctButton = document.querySelector(correct);
 }
 
-let correctButton = document.querySelector("#answer1")
+console.log("sdsdd" + correct);
+correctButton = document.querySelector(correct);
 
 correctButton.addEventListener("click", () => {
     alert("정답입니다! 와 짝짝짝짝짝 X 5")
