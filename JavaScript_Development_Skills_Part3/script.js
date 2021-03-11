29c9ebcacb02fe1e4d127c1b91606ce7504318ed
skysaver00 <skysaver00@naver.com>
2021-03-11 22:05:28 +0900

    2021-03-11

--- JavaScript_Development_Skills_Part3/script.js
+++ JavaScript_Development_Skills_Part3/script.js
@@ -1,9 +1,11 @@
 const temperature1 = [17, 21, 23]
 const temperature2 = [12, 5, -5, 0 ,4]
 
-const printForecast = (arr []) => {
-    const max = arr.length();
-    return max
+const printForecast = (arr = []) => {
+    for(let i = 0; i < arr.length; i++) {
+        console.log(`${arr[i]} in ${i + 1} days.`)
+    }
 }
 
-console.log(printForecast(temperature1))
+printForecast(temperature1)
+printForecast(temperature2)
