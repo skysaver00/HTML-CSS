ffa014a3495bf6c63d309ab3372ac2191f588aa3
skysaver00 <skysaver00@naver.com>
2021-03-11 22:01:22 +0900

    2021-03-11

---
+++ JavaScript_Development_Skills_Part3/script.js
@@ -0,0 +1,9 @@
+const temperature1 = [17, 21, 23]
+const temperature2 = [12, 5, -5, 0 ,4]
+
+const printForecast = (arr []) => {
+    const max = arr.length();
+    return max
+}
+
+console.log(printForecast(temperature1))
