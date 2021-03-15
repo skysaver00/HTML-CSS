c7b3e067307789554b15b5d4f701f674928c6acc
skysaver00 <skysaver00@naver.com>
2021-03-15 16:22:02 +0900

    2021-03-15

--- JavaScript_DOM_Events_Part5/Modal/script.js
+++ JavaScript_DOM_Events_Part5/Modal/script.js
@@ -11,13 +11,29 @@ const btnsOpenModal = document.querySelectorAll('.show-modal')
 //여기서 querySelector는 하나의 element만 가능하다. 따라서 다 고르게 all을 붙여줘야 한다.
 console.log(btnsOpenModal)
 
-for(let i = 0; i < btnsOpenModal.length; i++) {
-    console.log(btnsOpenModal[i].addEventListener('click', function() {
-        //class hidden을 없에주면 modal이 살아난다.
-        modal.classList.remove('hidden') //dot는 Selector에서만 붙는다.
-        //아직 모달을 지우지는 못한다.
-        overlay.classList.remove('hidden')
-    }))
+//class hidden을 없에주면 modal이 살아난다.
+//dot는 Selector에서만 붙는다.
+const openModal = function() {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
 }
+//얘는 부모 창을 overlay해준다.
+//가림막이라던가, 약간 흐리게 이런거
+const closeModal = function() {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden') //위 아래는 서로
+}//openModal, closeModal처럼 이렇게 던져주는 것도 가능하다.
+
+for(let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModal)
+    btnCloseModal.addEventListener('click', closeModal)
+}
+
+/* 이것을 기억하자!
+ * 1.querySelector를 통해 먼저 무엇을 선택해서 만들지 정한다.
+ * 2.여러개를 선택해야하는 클래스가 있으면 querySelectorAll을 쓴다.
+ * 3.All을 썼다면, for문으로 돌려준다. 그리고 addEventListener를 추가한다.
+ * 4.자유롭게 하고 싶으면 function을 따로 선언해 주어도 된다.
+ */
 
 //역시 클릭 이벤트를 만들려면, addEventListener를 만들어 줘야한다.
